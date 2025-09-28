d677437a2467942a648e9a8a56587f8174f9d083
Jayabalaji <jayabalaji711@gmail.com>
2025-09-28 10:58:38 +0530

    Update wsgi.py

--- Bank/wsgi.py
+++ Bank/wsgi.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import sys
 import os
 
+project_home = '/home/jayabalaji2k/Digital-Bank'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
 # set the settings module
 os.environ['DJANGO_SETTINGS_MODULE'] = 'Bank.settings'
